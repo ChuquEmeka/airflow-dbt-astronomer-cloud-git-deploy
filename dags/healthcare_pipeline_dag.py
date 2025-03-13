e67fb0553b0594cb86d5276617323338e858b6ec
ChuquEmeka <edeh.emeka84@gmail.com>
2025-03-13 14:08:59 +0100

    made several changes

--- dags/healthcare_pipeline_dag.py
+++ dags/healthcare_pipeline_dag.py
@@ -1,3 +1,4 @@
+###############################################################################
 
 from airflow.decorators import dag
 from pendulum import datetime
@@ -10,7 +11,6 @@ from airflow.models import Variable
 
 PATH_TO_DATA_SCRIPT = "/usr/local/airflow/include/raw_data_generation/healthcare_data.py"
 GCP_CONN_ID = Variable.get("GCP_CONN_ID", default_var="gcp")
-
 
 # Alternatively, Embed SQL directly instead of reading from file
 CREATE_EXTERNAL_TABLES_SQL = """
@@ -83,7 +83,10 @@ dbt_healthcare_pipeline()
 
 
 
-#################################################################
+
+
+####################################################################
+
 
 # from airflow.decorators import dag
 # from pendulum import datetime
@@ -91,7 +94,6 @@ dbt_healthcare_pipeline()
 # from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 # from airflow.models import Variable
 
-# ###########################################################
 # # DAG configuration
 
 # PATH_TO_DATA_SCRIPT = "/usr/local/airflow/include/raw_data_generation/healthcare_data.py"
@@ -104,7 +106,7 @@ dbt_healthcare_pipeline()
 #     CREATE_EXTERNAL_TABLES_SQL = f.read() 
   
 # @dag(
-#     schedule=None,
+#     schedule="0 0 * * *",  # Daily at midnight
 #     start_date=datetime(2024, 1, 1),
 #     catchup=False,
 #     tags=["healthcare"],
@@ -145,5 +147,76 @@ dbt_healthcare_pipeline()
  
 
 # dbt_healthcare_pipeline()
+
+#############################################################
+
+
+# from airflow.decorators import dag 
+# from pendulum import datetime
+# from airflow.operators.bash import BashOperator
+# from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+# from airflow.models import Variable
+# from cosmos.airflow.task_group import DbtTaskGroup
+# from dbt.healthcare_dbt_bigquery_data_pipeline.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+# from cosmos.constants import LoadMode
+# from cosmos.config import RenderConfig
+# PATH_TO_DATA_SCRIPT = "/usr/local/airflow/include/raw_data_generation/healthcare_data.py"
+# PATH_TO_SQL_SCRIPT = "/usr/local/airflow/include/raw_data_generation/create_external_tables.sql"
+# GCP_CONN_ID = Variable.get("GCP_CONN_ID", default_var="gcp")
+# TARGET_ENV = DBT_CONFIG.target_name
+
+# with open(PATH_TO_SQL_SCRIPT, "r") as f:
+#     CREATE_EXTERNAL_TABLES_SQL = f.read() \
+#         .replace("dev_healthcare_data", f"{TARGET_ENV}_healthcare_data") \
+#         .replace("/dev/", f"/{TARGET_ENV}/")
+
+
+# @dag(
+#     schedule="0 0 * * *",  # Daily at midnight
+#     start_date=datetime(2024, 1, 1),
+#     catchup=False,
+#     tags=["healthcare"],
+#     doc_md=__doc__,
+# )
+
+# def dbt_healthcare_pipeline():
+#     generate_data = BashOperator(
+#         task_id="generate_data",
+#         bash_command=f"python {PATH_TO_DATA_SCRIPT}"
+#     )
+
+#     create_external_tables = BigQueryInsertJobOperator(
+#         task_id="create_external_tables",
+#         configuration={
+#             "query": {
+#                 "query": CREATE_EXTERNAL_TABLES_SQL,
+#                 "useLegacySql": False,
+#             }
+#         },
+#         location="europe-west3",  # Matches BigQuery dataset location
+#         gcp_conn_id=GCP_CONN_ID,
+#     )
+
+    
+#     dbt_test_raw = BashOperator(
+#         task_id="dbt_test_raw",
+#         bash_command="source /usr/local/airflow/dbt_venv/bin/activate && dbt test --select source:*",
+#         cwd="/usr/local/airflow/include/dbt/healthcare_dbt_bigquery_data_pipeline"
+#     )
+        
+#     transform = DbtTaskGroup(
+#         group_id='transform',
+#         project_config=DBT_PROJECT_CONFIG,
+#         profile_config=DBT_CONFIG,  # Uses dynamic dbt profile from `cosmos_config.py`
+#         render_config=RenderConfig(
+#             load_method=LoadMode.DBT_LS,
+#             select=['path:models'],
+#             dbt_executable_path="source /usr/local/airflow/dbt_venv/bin/activate && /usr/local/airflow/dbt_venv/bin/dbt"
+#         )
+#     )
+
+#     generate_data >> create_external_tables >> dbt_test_raw >> transform
+# dbt_healthcare_pipeline()
+
 
 
